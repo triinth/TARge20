899e2e0f80bd71a789c6dc79b6c353fae096d669
Ingvar Derešivski <inde999@gmail.com>
2021-04-01 11:09:49 +0300

    DbContextInProgres

---
+++ TARge20.Data/TARge20DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARge20.Data
+{
+    public class TARge20DbContext : IdentityDbContext
+    {
+
+        public TARge20DbContext(DbContextOptions<TARge20DbContext> options)
+            : base(options) { }
+
+        // näide, kuidas teha, kui lisate domaini alla ühe objekti
+        //public DbSet<Employee> Employee { get; set; }
+    }
+}
